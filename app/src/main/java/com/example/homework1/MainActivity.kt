a9440e340c24c26f72a6c494c3e89b6a086841fb
grudzik <michal.grudz@gmail.com>
2019-08-11 18:12:24 +0200

    Activity based upon https://android.jlelse.eu/the-complete-android-splash-screen-guide-c7db82bce565 added.

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var counterTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
