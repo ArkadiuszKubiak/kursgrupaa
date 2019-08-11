401e099b3f981a2f70afa0890fe74aacec928cef
a.j.kubiak93@gmail.com <Kovloria159!>
2019-08-11 16:40:06 +0200

    Added fragments

--- app/src/main/java/com/example/homework1/MainActivity.kt
+++ app/src/main/java/com/example/homework1/MainActivity.kt
@@ -13,24 +13,21 @@ import android.widget.TextView
 
 const val REQUEST_ID = 1
 const val COUNTER_ID = "Counter"
-const val TEXT_CNT = "Second activity counter: "
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var CounterTextView :TextView
+    var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        CounterTextView = findViewById(R.id.cntTextView) as TextView
-        CounterTextView.setText(getString(R.string.Default_textView))
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if(requestCode == REQUEST_ID && resultCode == Activity.RESULT_OK)
         {
             if (data != null) {
-                val value = data.getIntExtra(COUNTER_ID,0)
-                CounterTextView.setText(getString(R.string.Default_text_content, value))
+                counter = data.getIntExtra(COUNTER_ID,0)
             }
         }
     }
@@ -38,39 +35,10 @@ class MainActivity : AppCompatActivity() {
     fun StartSecondActivity(@Suppress("UNUSED_PARAMETER")view: View?)
     {
         val intent = Intent(this, SecondActivity::class.java)
-        val cntTextView = CounterTextView.text.toString()
-        val pattern = "\\d+".toRegex()
-        val found = pattern.find(cntTextView)
-        if(found != null) {
-            intent.putExtra(COUNTER_ID, found.value.toInt())
-        }
-        else {
-            intent.putExtra(COUNTER_ID, 0)
-        }
-
+        intent.putExtra(COUNTER_ID, counter)
         startActivityForResult(intent, REQUEST_ID)
     }
 
-    override fun onSaveInstanceState(outState: Bundle?) {
-        if(outState != null) {
-            super.onSaveInstanceState(outState)
-            val cntTextView = CounterTextView.text.toString()
-            val pattern = "\\d+".toRegex()
-            val found = pattern.find(cntTextView)
-            if (found != null) {
-                outState.putInt(COUNTER_ID, found.value.toInt())
-            } else {
-                outState.putInt(COUNTER_ID, 0)
-            }
-        }
-    }
-
-    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        if(savedInstanceState != null) {
-            super.onRestoreInstanceState(savedInstanceState)
-                val value = savedInstanceState.getInt(COUNTER_ID)
-                CounterTextView.setText(getString(R.string.Default_text_content, value))
-
-        }
-    }
 }
+
+
--- app/src/main/java/com/example/homework1/SecondActivity.kt
+++ app/src/main/java/com/example/homework1/SecondActivity.kt
@@ -1,16 +1,36 @@
 package com.example.homework1
 
 import android.app.Activity
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity(), FirstFragment.OnFragmentInteractionListener, SecondFragment.OnFragmentInteractionListener  {
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_activity)
+
         counter  = intent.getIntExtra(COUNTER_ID, 0)
+
+        if (savedInstanceState == null) {
+            val manager = supportFragmentManager
+            val transaction = manager.beginTransaction()
+            transaction.replace(R.id.flContent, FirstFragment.newInstance(counter))
+            transaction.commit()
+        }
+        if (savedInstanceState == null) {
+            val manager = supportFragmentManager
+            val transaction = manager.beginTransaction()
+            transaction.replace(R.id.flContent2, SecondFragment.newInstance())
+            transaction.commit()
+        }
     }
 
     override fun onBackPressed() {
