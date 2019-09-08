021ccce449e2662bc668a39c3380e1f123643d55
a.j.kubiak93@gmail.com <Kovloria159!>
2019-09-08 21:48:22 +0200

    Fragment updated

---
+++ app/src/main/java/com/example/homework1/course/viewmodels/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.homework1.course.viewmodels
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    val LoginName = MutableLiveData<String>()
+
+}
